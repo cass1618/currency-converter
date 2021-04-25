210880803c9137e04cb1baace580d11901480633
cass1618 <cass16180@gmail.com>
2021-04-25 12:10:52 -0700

    Add codes class to handle additional API

---
+++ src/js/codes.js
@@ -0,0 +1,15 @@
+export default class Codes {
+  static async getCurrencyNames() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      else {
+        return response.json();
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+}
